2d1d52167bed7f04fa44884e631c8cd5fa4bbb65
hadeer <hadeeressam21@gmail.com>
2026-02-16 22:32:32 +0200

    update the application stack and add the style for each to navigation and display, and adding a new package for ICON display

--- src/stack/RootStack.tsx
+++ src/stack/RootStack.tsx
@@ -1,25 +1,46 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
+import { Ionicons } from "@react-native-vector-icons/ionicons"
 import ScreenStack from './ScreenStack'
 import { History, Notifications } from '../screens'
 import { RootParamList } from './types'
+import COLORS from '../constant/colors'
 
 
 const Tab = createBottomTabNavigator<RootParamList>()
 
 const RootStack = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen
-                name='home_flow'
-                component={ScreenStack}
-            />
+        <Tab.Navigator
+            initialRouteName='home_flow'
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: COLORS.PRIMARY,
+                tabBarInactiveTintColor: COLORS.SUBTLE,
+            }}
+        >
             <Tab.Screen
                 name='history'
+                options={{
+                    title: "History",
+                    tabBarIcon: ({ color, size }) => (<Ionicons name='calendar' color={color} size={size} />)
+                }}
                 component={History}
             />
             <Tab.Screen
+                name='home_flow'
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, size }) => (<Ionicons name='home' color={color} size={size} />)
+                }}
+                component={ScreenStack}
+            />
+            <Tab.Screen
                 name='norification'
+                options={{
+                    title: "Notifications",
+                    tabBarIcon: ({ color, size }) => (<Ionicons name='notifications' color={color} size={size} />)
+                }}
                 component={Notifications}
             />
         </Tab.Navigator>
--- src/stack/ScreenStack.tsx
+++ src/stack/ScreenStack.tsx
@@ -3,13 +3,20 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Home, Medication, Onboarding } from '../screens'
 import { ScreenParamList } from './types'
+import { userInfo } from '../utils/db'
 
 
 const Stack = createNativeStackNavigator<ScreenParamList>()
 
 const ScreenStack = () => {
+    const isAutherized = userInfo.getString("user_name") !== undefined
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            initialRouteName={isAutherized ? "home" : "onbording"}
+            screenOptions={{
+                headerShown: false
+            }}
+        >
             <Stack.Screen
                 name='onbording'
                 component={Onboarding}
